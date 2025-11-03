f26072b163464837dd1c120ff8fc9d750650121d
isakkhr <isakkhar@gmail.com>
2025-11-03 23:43:47 +0600

    database changed

--- Company/settings.py
+++ Company/settings.py
@@ -77,12 +77,25 @@ WSGI_APPLICATION = 'Company.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# PostgreSQL Database 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',  
+        'USER': 'postgres',  
+        'PASSWORD': '1234',  
+        'HOST': 'localhost', 
+        'PORT': '5432',
     }
 }
+
+# SQLite Database (backup - comment out when using PostgreSQL)
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
