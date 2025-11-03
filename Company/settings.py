9b59658b305f2c3d606dafd84df02b5c03dc393b
Sakkhar Saha <58117600+isakkhar@users.noreply.github.com>
2025-11-03 23:45:03 +0600

    Update database credentials in settings.py

--- Company/settings.py
+++ Company/settings.py
@@ -82,10 +82,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'blog',  
-        'USER': 'postgres',  
-        'PASSWORD': '1234',  
+        'USER': 'admin',  
+        'PASSWORD': 'password',  
         'HOST': 'localhost', 
-        'PORT': '5432',
+        'PORT': '',
     }
 }
 
